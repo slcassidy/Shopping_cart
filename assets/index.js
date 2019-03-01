7d778c84eaf53d7725c769ba0791944e793a85e5
slcassidy <samantha.l.cassidy@gmail.com>
2019-02-28 23:28:25 -0500

    Upload 20190228

--- assets/index.js
+++ assets/index.js
@@ -15,15 +15,7 @@ const shop =[
   }
 ];    //End of shopping list
 
-const cart = [
-    {
-        clothing: [],
-        electronics: [],
-        movies: [],
-        tools: [],
-        toys: [] 
-    }
-];
+
 
 //Check structure of the list
 console.log('shop', shop);
@@ -54,7 +46,7 @@ const itemRemoval = function(x){
 };
 
 // $("body").on('click', `.fa-minus-circle`, itemRemoval);
-$("body").on('click', '.remove', itemRemoval);
+$("tbody").on('click', '.remove', itemRemoval);
 
 
 
@@ -113,30 +105,15 @@ $("a").on("click", function(event){
 
     }); //End of Click
 
-// Sort the category
-$("#az").on("click", '.sort', function()
-{
-  $("#sort").tablesorter({
-    sortList: [[0,0]],
-    headers: {3:{sorter:false}}
-    // $("th").sorted.ascending
+
+// Search for the value
+
+$("#search").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#discover tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
   });
-
-//   $('thead th.sort').data('sortBy', function(th, td, tablesort) {
-  
-});
-// }
-// ); 
-
-// const find = function(){
-
-
-// };
-
-// $("body").on("click", '.choose', function(event){
- 
-
-// }
     
 
 
@@ -146,180 +123,45 @@ $("body").on("click", '.choose', function(event){
     const selection = $(this).index();
     console.log(selection);
 
-    // Alert if there is the same item
 
-    // if($("tr") != (this).val();
-    // if $("exist").val("click", '.choose', function(event){
-    // }
+    const tdVal = $("td").val();
+    console.log(`this is the detail of the file by TD ${tdVal}`)
+    const existVal = $("#exist").html();
+    console.log(`this is the detail of the file by ID ${existVal}`);
 
+    const val = $(this).val();
 
-   const cat = $(this).attr("category");
-   console.log(cat);
+    // Calling all the fields and adding them to the table
+    const cat = $(this).attr("category");
+    // console.log(cat);
 
-   const title = $(`<td id="sort">`).text(cat);
+    const title = $(`<td id="sort">`).text(cat);
 
-   const aName = $(this).html();
-   console.log(`This is the .html(): ${aName}`);
+    const aName = $(this).html();
+    // console.log(`This is the .html(): ${aName}`);
 
-   
-   const detail = $(`<td id="exist">`).text(aName);
-   console.log(`Tags on front ${detail}`);
-   
-   const tblBody = $(`tbody`);
+    
+    const detail = $(`<td id="exist">`).text(aName);
+    // console.log(`Tags on front ${detail}`);
+    
+    const tblBody = $(`tbody`);
 
-   const tblRow = $(`<tr class="remove">`);
- 
+    const tblRow = $(`<tr class="remove">`);
+    
 
-    const icon = $(`<td class="fas fa-minus-circle"></td>`);
+        const icon = $(`<td class="fas fa-minus-circle"></td>`);
+    
+    // Alert if there is the same item    
 
-   tblRow.append(title, detail, icon);
-//    Append the table row to the tbody element
-   tblBody.append(tblRow);
-
-//    if ($(this).val !=) 
-
-//    switch (idName) {
-//     case 0:
-//         tblRow.append(label);
-//         tblBody.append(tblRow);
-//         console.log(`Clothing Selected`);
-//         // type = "clothing";        
-//         break;
-//     case 1:
-//         text = "#clothDown";
-//         $('#contentType2').append(`<li>Movies Selected</li>`);
-//         // type = "movies";
-//         console.log(`Movies Selected`);
-//         break;
-//     case 2:
-//         text = "#toolsDown";
-//         $('#contentType2').append(`<li>Tools Selected</li>`);
-//         // type = "tools";  
-//         console.log(text);   
-//         break;
-//     case 3:
-//         text = "#electronicsDown";
-//         $('#contentType2').append(`<li>Electronics Selected</li>`);
-//         // type = "electronics"; 
-//         console.log(text);  
-//         break;
+    if(existVal === val){
+        alert("This item has already been selected, please choose another item");
+    }else{
+    tblRow.append(title, detail, icon);
+    //    Append the table row to the tbody element
+    tblBody.append(tblRow);
+    }
 
 });
-
-const before = $("td").val();
-console.log(before);
-
-//*****************************Testing other processes*****************************************************   
-
-// Testing the process by bringing in the ID
-//   Selecting Clothing directly
-
-//   $( "#clothDown" ).on("click",function() {
-//       clearContent();
-//     // $( "#log" ).append( "<div>Handler for .mouseover() called.</div>" );
-//     // console.log("Clothing Mouse Over");
-//     $('#category').append(`<h3>Clothing</h3>`);
-//     // Show the list of Clothing
-//     for( let i = 0; i < shop[0].items.clothing.length; i++ ) {
-//         $('#contentType2').append(`<li>${shop[0].items.clothing[i]} </li>`);
-//         // console.log(shop[0].items.clothing[i]);
-
-//      } 
-
-//   }); //End Click on
-
-//    Show the list of Electronics
-
-//   $( "#electronicsDown" ).on("click",function() {
-//         clearContent();
-//     $('#category').append(`<h3>Electronics</h3>`);
-//         for( let i = 0; i < shop[0].items.electronics.length; i++ ) {
-//             $('#contentType2').append(`<li>${shop[0].items.electronics[i]} </li>`);
-//             console.log(shop[0].items.electronics[i]);
-
-//         } //End of the for loop for electronics
-
-  
-// }); //End Click on
-
-    //   $('.dropdown-toggle').dropdown();
-
-// Case selection of the of the drop down **No longer going to utilize case
-
-// Case statement process:
-// $("a").each(function(i){
-//     $(this).on("click", {x:i}, function(event){
-//     //   alert("The " + $(this).index() + ". paragraph has data: " + event.data.x);
-//     //   const selected = event.data.value
-//     //   console.log(`This is the selected item: ${this} each ${selected}`);
-
-//     //   Case choosing what is selected
-//     const selection = $(this).index();
- 
-    
-//     const attribute = $(this).attr("id")
-//     console.log(attribute);
-
-//     const type = '';
-
-//     switch (selection) {
-// case 0:
-//     text = "toysDown";
-//     $('#contentType2').append(`<li>Clothing Selected</li>`);
-//     console.log(`Clothing Selected`);
-//     // type = "clothing";        
-//     break;
-// case 1:
-//     text = "#clothDown";
-//     $('#contentType2').append(`<li>Movies Selected</li>`);
-//     // type = "movies";
-//     console.log(`Movies Selected`);
-//     break;
-// case 2:
-//     text = "#toolsDown";
-//     $('#contentType2').append(`<li>Tools Selected</li>`);
-//     // type = "tools";  
-//     console.log(text);   
-//     break;
-// case 3:
-//     text = "#electronicsDown";
-//     $('#contentType2').append(`<li>Electronics Selected</li>`);
-//     // type = "electronics"; 
-//     console.log(text);  
-//     break;
-// case 4:
-//     text = "#toysDown";
-//     $('#contentType2').append(`<li>Electronics Selected</li>`);
-//     // type = "toys";  
-//     console.log(text);   
-//     break;
-    
-// default:
-//     text = "No value found new";
-//     console.log(`Not Working!!!`);
-//     }  //End of Case
-//     // console.log(type);
-
-//     }); //End of Click
-    
-
-//   }); //End of Each
-
-//  //Use toggle method.. -> https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_toggle
-//     // console.log(type);
-//     // // Show the list of Clothing
-//     // for( let i = 0; i < shop[0].items.type.length; i++ ) {
-//     //     $('#contentType2').append(`<li>${shop[0].items.type[i]} </li>`);
-//     //     // console.log(shop[0].items.clothing[i]);
-
-//     //  } 
-
-
-// Remove the items from the list using the removal icon
-// const removal = function() 
-// $(.fa-minus-circle)
-
-
 
 //End of document
 });
